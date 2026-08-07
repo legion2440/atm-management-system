06bd3835dd042d24030a251b7d851ab2e8bfb2a9
Nazar Yestayev <legion2440@mail.ru>
2026-08-07 11:18:27 +0500

    test: extend date and interest boundaries

--- tests/test_interest.c
+++ tests/test_interest.c
@@ -14,6 +14,9 @@ int main(void) {
     account.created = (Date){10, 10, 2012};
     account.balance = 1001.20;
 
+    account.type = ACCOUNT_CURRENT;
+    assert_money(account_interest(&account), 0.0);
+
     account.type = ACCOUNT_SAVINGS;
     assert_money(account_interest(&account), 5.8403333333);
 
@@ -34,11 +37,21 @@ int main(void) {
 
     assert(account_type_from_string("saving") == ACCOUNT_SAVINGS);
     assert(account_type_from_string("savings") == ACCOUNT_SAVINGS);
+    assert(account_type_from_string("unknown") == ACCOUNT_INVALID);
     assert(strcmp(account_type_name(ACCOUNT_FIXED03), "fixed03") == 0);
 
     Date leap;
     assert(parse_date("29/02/2024", &leap));
     assert(!parse_date("29/02/2023", &leap));
+    assert(!parse_date("31/04/2024", &leap));
+    assert(!parse_date("00/01/2024", &leap));
+    assert(!parse_date("01/13/2024", &leap));
+    assert(!parse_date("01-01-2024", &leap));
+
+    account.created = (Date){29, 2, 2024};
+    account.type = ACCOUNT_FIXED01;
+    maturity = account_interest_date(&account);
+    assert(maturity.day == 28 && maturity.month == 2 && maturity.year == 2025);
 
     puts("interest/date tests: OK");
     return 0;
